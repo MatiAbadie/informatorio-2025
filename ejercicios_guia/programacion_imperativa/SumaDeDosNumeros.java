22d87c08dba82b48de9bccf4b584ee2ba6abb1e1
MatiAbadie <matiasjojot7@gmail.com>
2025-05-17 22:01:30 -0300

    practicando primer commit

---
+++ ejercicios_guia/programacion_imperativa/SumaDeDosNumeros.java
@@ -0,0 +1,10 @@
+package ejercicios_guia.programacion_imperativa;
+
+public class SumaDeDosNumeros {
+    public static void main(String[] args){
+     int numero1 = 10;
+     int numero2 = 7;
+     int resultado = numero1 + numero2;
+        System.out.println("La suma es de: " + resultado);
+    }
+}
